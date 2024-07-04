d8d5e593685d4fef19e485702e5eba3dbdb8c62f
lakbot <lakshanthad@gmail.com>
2024-07-03 18:40:39 -0700

    Update

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -454,31 +454,3 @@ while (dice !== 6) {
   }
 }
 */
-
-const calcTip = function (bill) {
-  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-};
-
-/* Write your code below. Good luck! 🙂 */
-
-const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
-const tips = [];
-const totals = [];
-
-for (let i = 0; i < bills.length; i++) {
-  const tip = calcTip(bills[i]);
-  tips.push(tip);
-  totals.push(bills[i] + tip);
-}
-
-console.log(bills, tips, totals);
-
-const calcAverage = function (arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
-  }
-  return sum / arr.length;
-};
-
-console.log(calcAverage(totals));

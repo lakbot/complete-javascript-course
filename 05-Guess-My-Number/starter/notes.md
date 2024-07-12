f471d7dff38f5a4f51bbc8fceb1e18436602fb80
lakbot <lakshanthad@gmail.com>
2024-07-11 19:29:42 -0700

    Update

---
+++ 05-Guess-My-Number/starter/notes.md
@@ -0,0 +1,16 @@
+DOM - Document Object Model
+- Structured representation of html documents. allows javascript to access html elements and styles to manipulate them.
+- Change text, HTML attributes, CSS styles
+- DOM is automatically created by the browser as soon as the HTML page loads and it stores in a tree structure
+- Document is a special object that is the entry point to the DOM
+- DOM is not Javascript 
+- DOM and DOM methods are part of WEB APIs
+- DOM manipulation works the same on all browsers
+
+Event Listener
+- An event is something that happens on the page. ex: mouse click, mouse moving, key press
+
+
+
+
+
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+/* // querySelector() is a method available on document object
+console.log(document.querySelector('.message').textContent);
+// change the content inside the element
+document.querySelector('.message').textContent = '🎉Correct Number!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+// set value into input
+document.querySelector('.guess').value = 23;
+// get value from input
+console.log(document.querySelector('.guess').value); */
+
+// addEventListener method attaches an event handler with a function () value. This function will not load when JS loads but rather only when the event happens
+// 'click' is the name of the event we are listening for
+document.querySelector('.check').addEventListener('click', function () {
+  // convert string to number
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // first scenario is to assume there is no input
+  if (!guess){
+    document.querySelector('.message').textContent = '⛔No number!'
+  }
+});

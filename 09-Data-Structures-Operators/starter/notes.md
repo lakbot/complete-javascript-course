da069e274205ca06c2a3d2d58e1dc9631e36476a
lakbot <lakshanthad@gmail.com>
2024-07-22 22:49:56 -0700

    Update

---
+++ 09-Data-Structures-Operators/starter/notes.md
@@ -0,0 +1,4 @@
+Destructuring Arrays
+- Destructuring is an ES6 feature
+- Break a complex data structure down into a smaller data structure (variables)
+- Original array does not get affected by destructuring
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,8 +1,16 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+/*
+// Destructuring Arrays
+// without destructuring
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+// with destructuring
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
 
 // Data needed for first part of the section
 const restaurant = {
@@ -12,18 +20,45 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 };
+
+const [first, second] = restaurant.categories;
+console.log(first, second);
+
+// below the second element will be skipped
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// switching variables without destructuring
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main,secondary);
+
+// switching varibles with destructuring
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// Receive 2 return values from a function
+// Immediately create 2 variables out of one function call
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+// destructuring nested arrays
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// set default values for the variables when we are extracting them. That is going to be very useful in the case where we do not know the length of the array.
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
+*/
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';

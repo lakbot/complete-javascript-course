0b55cc95d2f2c6fa251a86dccb8327907a53b23c
Lakshantha <lakshanthad@gmail.com>
2024-08-07 11:45:21 -0700

    Update

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -704,6 +704,7 @@ planesInLine(5);
 planesInLine(3);
 */
 
+/*
 // String Methods Practice
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -718,3 +719,4 @@ for (const flight of flights.split('+')) {
   )} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(36);
   console.log(output);
 }
+  */
